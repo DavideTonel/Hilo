e7e642a34dcb56a682cdc77b6ee6a5df54729f0f
DavideTonel <davidetonelli17@gmail.com>
2025-03-24 18:00:04 +0100

    refactor: removed unused import

--- lib/navigation/app_router.dart
+++ lib/navigation/app_router.dart
@@ -1,6 +1,5 @@
 import 'package:go_router/go_router.dart';
 import 'package:roadsyouwalked_app/ui/pages/camera/camera_page.dart';
-import 'package:roadsyouwalked_app/ui/pages/camera/camera_preview_page.dart';
 import 'package:roadsyouwalked_app/ui/pages/entry_point/splash_page.dart';
 import 'package:roadsyouwalked_app/ui/pages/home/home_page.dart';
 
