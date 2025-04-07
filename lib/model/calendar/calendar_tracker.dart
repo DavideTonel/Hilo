10de074d7f2201e1b6eb31b302bbf1793f043490
DavideTonel <davidetonelli17@gmail.com>
2025-04-07 16:33:03 +0200

    fix: fix first day on calendar row

--- lib/model/calendar/calendar_tracker.dart
+++ lib/model/calendar/calendar_tracker.dart
@@ -2,65 +2,76 @@ import 'package:roadsyouwalked_app/model/calendar/calendar_day.dart';
 import 'package:roadsyouwalked_app/model/calendar/calendar_day_gap_type.dart';
 
 class CalendarTracker {
-  //final DateTime currentDate;
-  List<CalendarDay> _gapDaysPrevCurrentMonth = [];
-  List<CalendarDay> _gapDaysNextCurrentMonth = [];
-  List<CalendarDay> _daysCurrentMonth = [];
+  final int month;
+  final int year;
 
-  CalendarTracker(
-    int month,
-    int year
-  ) {
-    _daysCurrentMonth = _getDaysCurrentMonth(month, year);
-    _gapDaysPrevCurrentMonth = _getGapDaysPrevCurrentMonth(_daysCurrentMonth);
-    _gapDaysNextCurrentMonth = _getGapDaysNextCurrentMonth(_daysCurrentMonth);
+  late final List<CalendarDay> _gapDaysPrevCurrentMonth;
+  late final List<CalendarDay> _gapDaysNextCurrentMonth;
+  late final List<CalendarDay> _daysCurrentMonth;
+
+  CalendarTracker(this.month, this.year) {
+    _daysCurrentMonth = _generateDaysInMonth();
+    _gapDaysPrevCurrentMonth = _generateGapDaysBefore();
+    _gapDaysNextCurrentMonth = _generateGapDaysAfter();
   }
 
-  int _getNumOfDaysInMonth(int month, int year) {
-    return DateTime(year, month+1, 0).day;
-  }
+  List<CalendarDay> _generateDaysInMonth() {
+    final numDays = DateTime(year, month + 1, 0).day;
 
-  List<CalendarDay> _getDaysCurrentMonth(int month, int year) {
     return List.generate(
-      _getNumOfDaysInMonth(month, year),
-      (i) => CalendarDay(date: DateTime(year, month, i+1), gapType: CalendarDayGapType.currentMonth)
+      numDays,
+      (i) => CalendarDay(
+        date: DateTime(year, month, i + 1),
+        gapType: CalendarDayGapType.currentMonth,
+      ),
     );
   }
 
-  List<CalendarDay> _getGapDaysPrevCurrentMonth(List<CalendarDay> daysCurrentMonth) {
-    List<CalendarDay> gapDays = [];
-    DateTime firstDayOfMonth = daysCurrentMonth.first.date;
-    DateTime prevDay = firstDayOfMonth.add(const Duration(days: -1));
-    while (prevDay.weekday != 7) {    // 7 => sunday
+  List<CalendarDay> _generateGapDaysBefore() {
+    final List<CalendarDay> gapDays = [];
+
+    if (_daysCurrentMonth.isEmpty) return gapDays;
+
+    DateTime prevDay = _daysCurrentMonth.first.date.subtract(const Duration(days: 1));
+
+    while (prevDay.weekday != DateTime.monday) {
       gapDays.insert(
         0,
-        CalendarDay(
-          date: prevDay,
-          gapType: CalendarDayGapType.prevMonth
-        )
+        CalendarDay(date: prevDay, gapType: CalendarDayGapType.prevMonth),
       );
-      prevDay = prevDay.add(const Duration(days: -1));
+      prevDay = prevDay.subtract(const Duration(days: 1));
     }
+
+    gapDays.insert(
+      0,
+      CalendarDay(date: prevDay, gapType: CalendarDayGapType.prevMonth),
+    );
+
     return gapDays;
   }
 
-  List<CalendarDay> _getGapDaysNextCurrentMonth(List<CalendarDay> daysCurrentMonth) {
-    List<CalendarDay> gapDays = [];
-    DateTime lastDayOfMonth = daysCurrentMonth.last.date;
-    DateTime nextDay = lastDayOfMonth.add(const Duration(days: 1));
-    while (nextDay.weekday != 1) {    // 1 => monday
+  List<CalendarDay> _generateGapDaysAfter() {
+    final List<CalendarDay> gapDays = [];
+
+    if (_daysCurrentMonth.isEmpty) return gapDays;
+
+    DateTime nextDay = _daysCurrentMonth.last.date.add(const Duration(days: 1));
+
+    while (nextDay.weekday != DateTime.monday) {
       gapDays.add(
-        CalendarDay(
-          date: nextDay,
-          gapType: CalendarDayGapType.nextMonth
-        )
+        CalendarDay(date: nextDay, gapType: CalendarDayGapType.nextMonth),
       );
       nextDay = nextDay.add(const Duration(days: 1));
     }
+
     return gapDays;
   }
 
   List<CalendarDay> getDaysCurrentMonthWithGap() {
-    return [..._gapDaysPrevCurrentMonth, ..._daysCurrentMonth, ..._gapDaysNextCurrentMonth];
+    return [
+      ..._gapDaysPrevCurrentMonth,
+      ..._daysCurrentMonth,
+      ..._gapDaysNextCurrentMonth,
+    ];
   }
 }
